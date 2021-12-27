43a123742681426bb8d91b90271bcbdc8f184284
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2021-12-28 02:22:59 +0530

    feat: mock server call

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+//modifying url
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
 function clickHandler() {
-  outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
+  var inputText = txtInput.value; // taking input from textarea
+
+  // calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText; // output
+    });
 }
 
 btnTranslate.addEventListener("click", clickHandler);
