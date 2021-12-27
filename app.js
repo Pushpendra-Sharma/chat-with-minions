ba356f59b639acb94334dca65cdfcbdf6b51ed37
Pushpendra Sharma <pushpendrasharma400@gmail.com>
2021-12-28 02:38:12 +0530

    fix: actual server call

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var mockServerURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-//modifying url
 function getTranslationURL(input) {
   return serverURL + "?" + "text=" + input;
 }
 
 function clickHandler() {
-  var inputText = txtInput.value; // taking input from textarea
+  var inputText = txtInput.value; // taking input
 
-  // calling server for processing
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
       outputDiv.innerText = translatedText; // output
+    })
+    .catch((error) => {
+      console.log("error occured", error);
+      alert("something wrong with server! try again after some time");
     });
 }
 
